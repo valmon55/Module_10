3cb767e65fbebdb11dc74eaa01738d1e20698a6a
valmon55 <fedor_ka@mail.ru>
2023-07-16 14:40:03 +0300

    Task2

---
+++ CS_Basic/CS_Basic.Task_2/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace CS_Basic.Task_2
+{
+    public interface ISum 
+    {
+        double Sum(double a, double b);
+    }
+    public class Calculator : ISum
+    {
+        public double Sum(double a, double b)
+        {
+            Console.WriteLine($"Результат = {a + b}");
+            return (a + b);
+        }
+    }
+    public class Keyboard
+    {
+        public double a { get; private set; }
+        public double b { get; private set; }
+
+        public delegate double TwoNumEnter(double a, double b);
+
+        public event TwoNumEnter TwoNumEnterEvent;
+
+        public void GetTwoNumbers()
+        {
+            while (true)
+            {
+                try
+                {
+                    a = NumberEntered();
+                    b = NumberEntered();
+                    TwoNumEnterEvent?.Invoke(a, b);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+        public double NumberEntered()
+        {
+            Console.Write("Введите число: ");
+            return Convert.ToDouble(Console.ReadLine());
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            Keyboard keyboard = new Keyboard();
+            keyboard.TwoNumEnterEvent += calc.Sum;
+
+            keyboard.GetTwoNumbers();
+
+            Console.ReadKey();
+        }
+
+    }
+}
