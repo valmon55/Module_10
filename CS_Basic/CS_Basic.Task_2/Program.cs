32973219f189ede44b46c2f82e0c1ea687287564
valmon55 <fedor_ka@mail.ru>
2023-07-16 15:00:59 +0300

    +event color

--- CS_Basic/CS_Basic.Task_2/Program.cs
+++ CS_Basic/CS_Basic.Task_2/Program.cs
@@ -1,13 +1,41 @@
 ﻿namespace CS_Basic.Task_2
 {
+    public interface ILogger
+    {
+        void Event(string message);
+        void Error(string message);
+    }
+    public class Logger : ILogger
+    {
+        public void Error(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+        }
+
+        public void Event(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(message);
+        }
+    }
+
     public interface ISum 
     {
         double Sum(double a, double b);
     }
     public class Calculator : ISum
     {
+        ILogger logger { get; }
+
+        public Calculator(ILogger logger)
+        {
+            this.logger = logger;
+        }
+
         public double Sum(double a, double b)
         {
+            logger.Event($"Результат = {a + b}");
             Console.WriteLine($"Результат = {a + b}");
             return (a + b);
         }
